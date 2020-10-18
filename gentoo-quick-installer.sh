aa9ed3c33c59d1389ccfb6f19e004b118bdd54ac
Artem Butusov <art.sormy@gmail.com>
2020-10-18 02:37:36 -0400

    Fix gentoo-quick-installer.sh to work with latest Gentoo

--- gentoo-quick-installer.sh
+++ gentoo-quick-installer.sh
@@ -1,27 +1,46 @@
-#!/bin/bash
+#!/bin/sh
+
+##
+# GENTOO INSTALLER
+#
+# Read more: http://www.artembutusov.com/gentoo-linux-quick-installer-script/
+#
+# Usage:
+#
+# export OPTION1=VALUE1
+# export OPTION2=VALUE2
+# ./gentoo-quick-installer.sh
+#
+# Options:
+#
+# USE_LIVECD_KERNEL - 1 to use livecd kernel (saves time) or 0 to build kernel (takes time)
+# SSH_PUBLIC_KEY - ssh public key, pass contents of `cat ~/.ssh/id_rsa.pub` for example
+# ROOT_PASSWORD - root password, only SSH key-based authentication will work if not set
+##
 
 set -e
 
-GENTOO_RELEASES_URL=http://distfiles.gentoo.org/releases
+GENTOO_MIRROR="http://distfiles.gentoo.org"
 
-GENTOO_ARCH=amd64
-GENTOO_VARIANT=amd64
+GENTOO_ARCH="amd64"
+GENTOO_STAGE3="amd64"
 
 TARGET_DISK=/dev/sda
 
-PARTITION_BOOT_SIZE=100M
-PARTITION_SWAP_SIZE=1G
+TARGET_BOOT_SIZE=100M
+TARGET_SWAP_SIZE=1G
 
 GRUB_PLATFORMS=pc
 
 USE_LIVECD_KERNEL=${USE_LIVECD_KERNEL:-1}
 
 SSH_PUBLIC_KEY=${SSH_PUBLIC_KEY:-}
+ROOT_PASSWORD=${ROOT_PASSWORD:-}
 
 echo "### Checking configuration..."
 
-if [ -z "$SSH_PUBLIC_KEY" ]; then
-    echo "SSH_PUBLIC_KEY variable is not set"
+if [ -z "$SSH_PUBLIC_KEY" ] && [ -z "$ROOT_PASSWORD" ]; then
+    echo "SSH_PUBLIC_KEY or ROOT_PASSWORD must be set to continue"
     exit 1
 fi
 
@@ -32,8 +51,8 @@ ntpd -gq
 echo "### Creating partitions..."
 
 sfdisk ${TARGET_DISK} << END
-size=$PARTITION_BOOT_SIZE,bootable
-size=$PARTITION_SWAP_SIZE
+size=$TARGET_BOOT_SIZE,bootable
+size=$TARGET_SWAP_SIZE
 ;
 END
 
@@ -65,25 +84,24 @@ cd /mnt/gentoo
 
 echo "### Installing stage3..."
 
-STAGE3_PATH_URL=$GENTOO_RELEASES_URL/$GENTOO_ARCH/autobuilds/latest-stage3-$GENTOO_VARIANT.txt
-STAGE3_PATH=$(curl -s $STAGE3_PATH_URL | grep -v "^#" | cut -d" " -f1)
-STAGE3_URL=$GENTOO_RELEASES_URL/$GENTOO_ARCH/autobuilds/$STAGE3_PATH
+STAGE3_PATH_URL="$GENTOO_MIRROR/releases/$GENTOO_ARCH/autobuilds/latest-stage3-$GENTOO_STAGE3.txt"
+STAGE3_PATH=$(curl -s "$STAGE3_PATH_URL" | grep -v "^#" | cut -d" " -f1)
+STAGE3_URL="$GENTOO_MIRROR/releases/$GENTOO_ARCH/autobuilds/$STAGE3_PATH"
 
-wget $STAGE3_URL
+wget "$STAGE3_URL"
 
-tar xvpf $(basename $STAGE3_URL)
+tar xvpf "$(basename "$STAGE3_URL")" --xattrs-include='*.*' --numeric-owner
+
+rm -fv "$(basename "$STAGE3_URL")"
 
 if [ "$USE_LIVECD_KERNEL" != 0 ]; then
     echo "### Installing LiveCD kernel..."
 
-    LIVECD_KERNEL_VERSION=$(cat /proc/version | cut -d" " -f3)
-    KERNEL_ARCH_SUFFIX=$(echo $GENTOO_ARCH | sed "s/^amd64$/x86_64/")
+    LIVECD_KERNEL_VERSION=$(cut -d " " -f 3 < /proc/version)
 
-    cp -v /mnt/cdrom/boot/gentoo \
-        /mnt/gentoo/boot/kernel-genkernel-$KERNEL_ARCH_SUFFIX-$LIVECD_KERNEL_VERSION
-    cp -v /mnt/cdrom/boot/gentoo.igz \
-        /mnt/gentoo/boot/initramfs-genkernel-$KERNEL_ARCH_SUFFIX-$LIVECD_KERNEL_VERSION
-    cp -vR /lib/modules/$LIVECD_KERNEL_VERSION /mnt/gentoo/lib/modules/
+    cp -v "/mnt/cdrom/boot/gentoo" "/mnt/gentoo/boot/vmlinuz-$LIVECD_KERNEL_VERSION"
+    cp -v "/mnt/cdrom/boot/gentoo.igz" "/mnt/gentoo/boot/initramfs-$LIVECD_KERNEL_VERSION.img"
+    cp -vR "/lib/modules/$LIVECD_KERNEL_VERSION" "/mnt/gentoo/lib/modules/"
 fi
 
 echo "### Installing kernel configuration..."
@@ -105,23 +123,25 @@ LABEL=swap none  swap sw             0 0
 LABEL=root /     ext4 noatime        0 1
 END
 
-echo "### Mounting proc/sys/dev/pts..."
+echo "### Mounting proc/sys/dev..."
 
 mount -t proc none /mnt/gentoo/proc
 mount -t sysfs none /mnt/gentoo/sys
 mount -o bind /dev /mnt/gentoo/dev
 mount -o bind /dev/pts /mnt/gentoo/dev/pts
+mount -o bind /dev/shm /mnt/gentoo/dev/shm
 
 echo "### Changing root..."
 
-cat > /mnt/gentoo/root/gentoo-init.sh << END
+chroot /mnt/gentoo /bin/bash -s << END
 #!/bin/bash
 
 set -e
 
 echo "### Upading configuration..."
 
-env-update && source /etc/profile
+env-update
+source /etc/profile
 
 echo "### Installing portage..."
 
@@ -169,31 +189,28 @@ echo "### Configuring network..."
 ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
 rc-update add net.eth0 default
 
-echo "### Configuring SSH..."
+if [ -z "$ROOT_PASSWORD" ]; then
+    echo "### Removing root password..."
+    passwd -d -l root
+else
+    echo "### Configuring root password..."
+    echo "root:$ROOT_PASSWORD" | chpasswd
+fi
 
-rc-update add sshd default
+if [ -n "$SSH_PUBLIC_KEY" ]; then
+    echo "### Configuring SSH..."
 
-passwd -d -l root
+    rc-update add sshd default
 
-mkdir /root/.ssh
-touch /root/.ssh/authorized_keys
-chmod 750 /root/.ssh
-chmod 640 /root/.ssh/authorized_keys
-echo "$SSH_PUBLIC_KEY" > /root/.ssh/authorized_keys
+    mkdir /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 750 /root/.ssh
+    chmod 640 /root/.ssh/authorized_keys
+    echo "$SSH_PUBLIC_KEY" > /root/.ssh/authorized_keys
+fi
 END
-
-chmod +x /mnt/gentoo/root/gentoo-init.sh
-
-chroot /mnt/gentoo /root/gentoo-init.sh
-
-echo "### Cleaning..."
-
-rm /mnt/gentoo/$(basename $STAGE3_URL)
-rm /mnt/gentoo/root/gentoo-init.sh
 
 echo "### Rebooting..."
 
-# signal that installation is completed before rebooting
-for i in `seq 1 10`; do tput bel; done
-
+eject || true
 reboot
